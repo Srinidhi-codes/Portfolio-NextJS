6d631c10939410d3b6ba5b5bfff9e8ae07503b27
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2024-03-03 22:44:00 +0530

    Created tsparticles animation

--- src/app/components/Circles.js
+++ src/app/components/Circles.js
@@ -1,5 +1,14 @@
-const Circles = () => {
-  return <div>Circles</div>;
-};
+import Image from 'next/image'
+import React from 'react'
 
-export default Circles;
+function Circles() {
+  return (
+    <>
+      <div className='w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10'>
+        <Image src={'/assests/circles.png'} width={260} height={200} className=' w-full h-full' alt="" />
+      </div>
+    </>
+  )
+}
+
+export default Circles
--- src/app/components/Socials.js
+++ src/app/components/Socials.js
@@ -1,5 +1,24 @@
-const Socials = () => {
-  return <div>Socials</div>;
-};
+import Link from 'next/link'
+import React from 'react'
+import { RiGithubLine, RiInstagramLine, RiLinkedinFill, RiYoutubeLine } from "react-icons/ri"
 
-export default Socials;
+const SocialIcons = [
+  { path: '/', icon: <RiYoutubeLine /> },
+  { path: '/', icon: <RiInstagramLine /> },
+  { path: '/', icon: <RiLinkedinFill /> },
+  { path: '/', icon: <RiGithubLine /> },
+]
+
+function Socials() {
+  return (
+    <>
+      <div className='flex items-center gap-x-5 text-lg text-white'>
+        {SocialIcons.map((element, idx) => <Link key={idx} href={element.path} className='hover:text-accent transition-all duration-300'>
+          {element.icon}
+        </Link>)}
+      </div>
+    </>
+  )
+}
+
+export default Socials
